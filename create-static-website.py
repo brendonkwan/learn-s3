92c95effa07e40c4562a7a406ac80d09089eeb5b
d288578 <kwan.brendon@gmail.com>
2018-03-10 22:46:04 +1100

    Static website creation script working, but static website's index and error pages still need to be made publicly accessible.

---
+++ create-static-website.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import boto3
+import botocore
+
+BUCKET_WEBSITE = 'static.brendonkwan.com'
+INDEX_FILE = 'static-website/index.txt'
+INDEX_KEY = 'index.txt'
+ERROR_FILE = 'static-website/error.txt'
+ERROR_KEY = 'error.txt'
+REGION = 'ap-southeast-2'
+
+s3 = boto3.resource('s3')
+
+print('Creating bucket website "%s".' % (BUCKET_WEBSITE))
+
+bucket_website = s3.BucketWebsite(BUCKET_WEBSITE)
+
+try:
+    bucket_website.Bucket().create(
+        CreateBucketConfiguration={
+            'LocationConstraint': REGION
+        }
+    )
+except Exception as e:
+    print('Exception: %s' % e)
+    
+print('Setting index document to key "%s" and error document to key "%s".' %
+    (INDEX_KEY, ERROR_KEY))
+
+bucket_website.put(
+    WebsiteConfiguration={
+        'IndexDocument': {'Suffix': INDEX_KEY},
+        'ErrorDocument': {'Key': ERROR_KEY}
+    }
+)
+
+print('Make the bucket''s contents publicly readable')
+
+bucket_website.Bucket().Acl().put(ACL='public-read')
+
+print('Uploading index file "%s" to key "%s".' % (INDEX_FILE, INDEX_KEY))
+bucket_website.Bucket().upload_file(INDEX_FILE, INDEX_KEY)
+
+print('Uploading error file "%s" to key "%s".' % (ERROR_FILE, ERROR_KEY))
+bucket_website.Bucket().upload_file(ERROR_FILE, ERROR_KEY)
+
+print('Static website is available at http://%s.s3-website.%s.aws.amazon.com' %
+    (BUCKET_WEBSITE, REGION))
