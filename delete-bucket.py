013e918ee6553a8c2fefb8bc3082cddde25f5f8f
Blastfarian <blastafarian@github.com>
2018-03-12 19:48:50 +1100

    Fixed bugs with deletion code

--- delete-bucket.py
+++ delete-bucket.py
@@ -2,34 +2,54 @@
 
 import argparse
 import boto3
+import http
 import pprint
 
-def list_keys_of_bucket_objects(bucket):
+def list_keys_of_bucket_objects(bucket_name):
     """
     Returns a list that contains the key of each of the bucket's objects.
     If the bucket is empty, returns an empty list.
-    If an error was encountered when reading the bucket, returns None.
     NOTE: Limited to returning keys of first 1000 bucket objects.
-    TODO: Improve function to list all bucket objects.
+    TODO: Improve function to check for more pages of results.
     """
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.list_objects(Bucket=bucket)
-        if len(response['Contents']) == 0:
-            return None
+    
+    print('Looking for the objects of bucket "%s"' % bucket_name)
+    response = boto3.client('s3').list_objects(Bucket=bucket_name)
+    if 'Contents' not in response:
+        return []
+    else:
+        return [obj['Key'] for obj in response['Contents']]
+
+
+def delete_objects_of_bucket(bucket_name, test_mode):
+    for key in list_keys_of_bucket_objects(bucket_name):
+        if test_mode:
+            print('Would delete object with key "%s" from bucket "%s"'
+                    % (key, bucket_name))
         else:
-            return [obj['Key'] for obj in response['Contents']]
-    except Exception as e:
-        print('The following exception occurred when listing bucket ''%s'': ' %
-                bucket, end='')
-        pprint.pprint(e)
-        return None
+            print('Deleting object with key "%s" from bucket "%s"' %
+                    (key, bucket_name))
+            object = boto3.resource('s3').Object(bucket_name=bucket_name,
+                    key=key)
+            response = object.delete()
+            if response['ResponseMetadata']['HTTPStatusCode'] != http.HTTPStatus.NO_CONTENT:
+                print('Error during deletion. Response was:')
+                pprint.pprint(response)
+
+
+def delete_bucket(bucket_name, test_mode):
+    if test_mode:
+        print('Would delete bucket "%s"' % bucket_name)
+    else:
+        print('Deleting bucket "%s"' % bucket_name)
+        boto3.resource('s3').Bucket(bucket_name).delete()
+
 
 def main():
     parser = argparse.ArgumentParser(description='Deletes buckets from S3')
-    parser.add_argument('buckets',
+    parser.add_argument('bucket_names',
             help='Name of a bucket to delete',
-            metavar='B',
+            metavar='BUCKET_NAME',
             nargs='+')
     parser.add_argument('--test',
             help='Test mode. If specified, no deletions will be made',
@@ -37,34 +57,12 @@ def main():
             default=False)
     args = parser.parse_args()
 
-    s3_resource = boto3.resource('s3')
+    for bucket_name in args.bucket_names:
+        # All objects in the bucket must be deleted before the bucket itself
+        # can be deleted.
+        delete_objects_of_bucket(bucket_name, args.test)
+        delete_bucket(bucket_name, args.test)
 
-    for bucket in args.buckets:
-
-        keys = list_keys_of_bucket_objects(bucket)
-
-        if keys is None:
-            print('Leaving bucket ''%s'' alone' % bucket)
-            continue
-
-        for key in keys:
-            if args.test:
-                print('Would delete object with key ''%s'' from bucket ''%s'''
-                        % (key, bucket))
-            else:
-                print('Deleting object with key ''%s'' from bucket ''%s''' %
-                        (key, bucket))
-                object = s3_resource.Object(bucket_name=bucket, key=key)
-                response = object.delete()
-                print('Deletion response was: ', end='')
-                pprint.pprint(response)
-
-        if args.test:
-            print('Would delete bucket ''%s''' % bucket)
-        else:
-            print('Deleting bucket ''%s''' % bucket)
-            resource = boto3.resource('s3')
-            resource.Bucket(bucket).delete()
 
 if __name__ == '__main__':
     main()
